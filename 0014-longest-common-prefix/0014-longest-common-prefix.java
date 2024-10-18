8ff7c2c76f3c164bd3838a83ccfbf1c007cdaa64
Nicola Ranzolin <104451140+nRanzo@users.noreply.github.com>
2024-10-18 11:43:05 +0200

    Time: 0 ms (100%), Space: 41 MB (97.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,17 +1,10 @@
-// sorting means O(nmlog(n) which is fine, string length is <= 200
-class Solution {
+public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String s1 = strs[0];
-        String s2 = strs[strs.length-1];
-        int idx = 0;
-        while (idx < s1.length() && idx < s2.length()){
-            if (s1.charAt(idx) == s2.charAt(idx)){
-                idx++;
-            } else {
-                break;
-            }
-        }
-        return s1.substring(0, idx);
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
     }
 }
