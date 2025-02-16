4c941e0b5b68df8ea4f8a3744d4fc70c11bea530
Nicola Ranzolin <104451140+nRanzo@users.noreply.github.com>
2025-02-16 18:31:44 +0100

    Time: 586 ms (55.52%), Space: 20.8 MB (23.72%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue            # skip duplicates
+            
+            left, right = i+1, n-1  # two pointers
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    # skipping duplicates        
+                    while left < right and nums[left] == nums[left+1]:
+                        left +=1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -=1
+
+                    # moving both pointers after finding valid tuplet
+                    left += 1
+                    right -= 1
+
+                elif total < 0:
+                    left += 1
+                
+                else:
+                    right -= 1
+        return res
