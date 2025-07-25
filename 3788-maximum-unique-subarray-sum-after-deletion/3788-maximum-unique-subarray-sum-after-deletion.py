8065fa0bbf2cf8a4bca1f2f4b7c2529ab5ad831d
Nicola Ranzolin <104451140+nRanzo@users.noreply.github.com>
2025-07-25 16:52:23 +0200

    Time: 0 ms (100%), Space: 17.9 MB (30.68%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -0,0 +1,10 @@
+class Solution:
+    def remove_neg(self, lista: List[int]) -> List[int]:
+        filtered = [x for x in lista if x > 0]
+        return filtered if filtered else [max(lista)]
+
+    def maxSum(self, nums: List[int]) -> int:
+        nums = self.remove_neg(nums)        # removes more items possible O(n)
+        nums = list(set(nums))         # removes duplicates in O(n)
+        nums.sort()                    # order less items possible in O(n log n)
+        return sum(nums)
